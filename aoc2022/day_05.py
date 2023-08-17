81270e62dcb358f2f43ae01f7f573ab6205461b7
Alexander Garber <clockworkpc@gmail.com>
2023-08-17 18:03:15 +0300

    Day 05 Part 1

---
+++ aoc2022/day_05.py
@@ -0,0 +1,127 @@
+"""Day 05"""
+
+import re
+import pprint
+from collections import Counter
+
+pp = pprint.PrettyPrinter(indent=4)
+# Part 01
+
+
+def readlines(path, split=True):
+    with open(path, "r") as file:
+        if split:
+            return [line.split(" ") for line in file]
+        else:
+            return [line for line in file]
+
+
+def filter_by_pattern(pattern, lines):
+    return list(filter(lambda line: re.match(pattern, line), lines))
+
+
+def new_map_sans_crates(lines):
+    def is_header(line):
+        return line[0] == "" and line[1] == "1"
+
+    header_ary = list(filter(lambda line: is_header(line), lines))[0]
+    key_ary = [item.strip() for item in header_ary if re.match(r"\d", item)]
+
+    map_dict = {}
+    for key in key_ary:
+        map_dict[key] = []
+
+    return map_dict
+
+
+def extract_distinct_elements(line):
+    distinct_elements = []
+    seen_elements = set()
+
+    for element in line:
+        stripped_element = element.strip()
+        if stripped_element and stripped_element not in seen_elements:
+            seen_elements.add(stripped_element)
+            distinct_elements.append(element)
+
+    return distinct_elements
+
+
+def find_crate_lines(path):
+    with open(path, "r") as file:
+        lines = []
+        pattern = r"\[[A-Z]\]"
+        for line in file:
+            if re.search(pattern, line):
+                clean_line = line.replace("\n", "")
+                joined = "".join(clean_line)
+                corrected = joined.replace("    ", "#")
+                chars = [c for c in corrected if re.match(r"[A-Z#]", c)]
+                lines.append(chars)
+
+        return lines
+
+
+def new_map_with_crates(path, empty_map):
+    crate_ary_ary = find_crate_lines(path)
+
+    for crate_ary in crate_ary_ary:
+        for index, char in enumerate(crate_ary, 1):
+            key = str(index)
+
+            if char == "#":
+                continue
+
+            empty_map[key].append(char)
+
+    return empty_map
+
+
+def import_map(path):
+    lines = readlines(path)
+    empty_map = new_map_sans_crates(lines)
+    return new_map_with_crates(path, empty_map)
+
+
+def move_crate(my_map, cmd):
+    def extract_substring(word, integer=True):
+        string = re.search(r"\b{}\b \d+".format(word), cmd).group()
+        number = re.search(r"\d+", string).group()
+        return int(number) if integer else number
+
+    move = extract_substring("move")
+    origin_str = extract_substring("from", False)
+    destination_str = extract_substring("to", False)
+
+    origin = my_map[origin_str]
+    destination = my_map[destination_str]
+
+    for _ in range(move):
+        crate = origin.pop(0)
+        destination.insert(0, crate)
+    return my_map
+
+
+def import_instructions(path):
+    lines = readlines(path, split=False)
+    instructions = []
+    for line in lines:
+        if re.findall("move", line):
+            instructions.append(line.strip())
+    return instructions
+
+
+def top_crates_from_instructions(path):
+    my_map = import_map(path)
+    instructions = import_instructions(path)
+
+    for i in instructions:
+        move_crate(my_map, i)
+
+    return top_crates(my_map)
+
+
+def top_crates(my_map):
+    crates = list(map(lambda ary: ary[0], my_map.values()))
+    joined = "".join(crates)
+    return joined
---
+++ tests/test_day_05.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from aoc2022 import day_05 as d5
+
+import json
+
+INPUT_TEST_PATH = "tests/input_files/day_05_input_sample.txt"
+INPUT_FULL_PATH = "tests/input_files/day_05_input.txt"
+
+"""PART 01"""
+
+
+def test_new_map_empty():
+    lines = d5.readlines(INPUT_TEST_PATH)
+    res = d5.new_map_sans_crates(lines)
+    assert res == {"1": [], "2": [], "3": []}
+
+
+def test_new_map_populated():
+    lines = d5.readlines(INPUT_TEST_PATH)
+    empty_map = d5.new_map_sans_crates(lines)
+    res = d5.new_map_with_crates(INPUT_TEST_PATH, empty_map)
+    assert res == {"1": ["N", "Z"], "2": ["D", "C", "M"], "3": ["P"]}
+
+
+def test_import_map():
+    res = d5.import_map(INPUT_TEST_PATH)
+    assert res == {"1": ["N", "Z"], "2": ["D", "C", "M"], "3": ["P"]}
+
+
+def test_move_crate():
+    my_map = d5.import_map(INPUT_TEST_PATH)
+
+    cmd = "move 1 from 2 to 1"
+    res = d5.move_crate(my_map, cmd)
+    assert res == {"1": ["D", "N", "Z"], "2": ["C", "M"], "3": ["P"]}
+
+    cmd = "move 3 from 1 to 3"
+    res = d5.move_crate(my_map, cmd)
+    assert res == {"1": [], "2": ["C", "M"], "3": ["Z", "N", "D", "P"]}
+
+    cmd = "move 2 from 2 to 1"
+    res = d5.move_crate(my_map, cmd)
+    assert res == {"1": ["M", "C"], "2": [], "3": ["Z", "N", "D", "P"]}
+
+    cmd = "move 1 from 1 to 2"
+    res = d5.move_crate(my_map, cmd)
+    assert res == {"1": ["C"], "2": ["M"], "3": ["Z", "N", "D", "P"]}
+
+
+def test_top_crates():
+    my_map = {"1": ["C"], "2": ["M"], "3": ["Z", "N", "D", "P"]}
+    res = d5.top_crates(my_map)
+    assert res == "CMZ"
+
+
+def test_import_instructions():
+    res = d5.import_instructions(INPUT_TEST_PATH)
+    text = [
+        "move 1 from 2 to 1",
+        "move 3 from 1 to 3",
+        "move 2 from 2 to 1",
+        "move 1 from 1 to 2",
+    ]
+    assert res == text
+
+
+def test_top_crates_from_instructions():
+    res = d5.top_crates_from_instructions(INPUT_TEST_PATH)
+    assert res == "CMZ"
+
+    res = d5.top_crates_from_instructions(INPUT_FULL_PATH)
+    assert res == "BSDMQFLSP"
