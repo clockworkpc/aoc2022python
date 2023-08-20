76ea6c727cfe2532795bac75f748eda43e7fa179
Alexander Garber <clockworkpc@gmail.com>
2023-08-20 18:34:19 +0300

    dictionary for fs

--- aoc2022/day_07.py
+++ aoc2022/day_07.py
@@ -4,21 +4,53 @@ import re
 
 
 def classify_line(line):
-    first_cha
-    if line[0] == "$":
-        return "command"
+    def regex_test(pattern, value):
+        if re.match(pattern, line):
+            return value
 
-    if re.match(r"^\d+", line):
-        return "file"
+    return (
+        regex_test(r"^\$", "command")
+        or regex_test(r"^\d+", "file")
+        or regex_test(r"^dir", "directory")
+    )
 
-    if re.match(r"^\dir", line):
-        return "directory"
+
+# def get_nested_value(dictionary, keys):
+#     if len(keys) == 0:
+#         return dictionary
+#     else:
+#         return get_nested_value(dictionary[keys[0]], keys[1:])
+
+# nested_keys = ['first_level', 'second_level', 'third_level']
+# result = get_nested_value(nested_dict, nested_keys)
+
+
+def find_directories(fs, parent_key="/"):
+    nested_keys = []
+    items = fs.items()
+    print(items)
+    for key, value in fs.items():
+        if isinstance(value, dict):
+            nested_keys.append(key)
+            print(f"KEY: {key}, VALUE: {value}")
+            nested_keys.extend(find_directories(value, parent_key))
+        # else:
+        #     nested_keys.append(parent_key)
+    return nested_keys
 
 
 def create_fs_dictionary(terminal_output):
+    def directory_name(line):
+        return line.split(" ")[1]
+
     file_system = {}
     lines = terminal_output.split("\n")
 
+    cwd = file_system
+
     for line in lines:
         line_type = classify_line(line)
+        if line_type == "directory":
+            dirname = directory_name(line)
+
     return file_system
--- tests/test_day_07.py
+++ tests/test_day_07.py
@@ -6,15 +6,46 @@ INPUT_TEST_PATH = "tests/input_files/day_07_input_sample.txt"
 INPUT_FULL_PATH = "tests/input_files/day_07_input.txt"
 
 
-def test_create_fs_dictionary():
-    terminal_output = """$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d"""
-    fs_dictionary = {
+def test_classify_line():
+    line = "$ ls"
+    res = d7.classify_line(line)
+    assert res == "command"
+
+    line = "dir a"
+    res = d7.classify_line(line)
+    assert res == "directory"
+
+    line = "dir d"
+    res = d7.classify_line(line)
+    assert res == "directory"
+
+    line = "14848514 b.txt"
+    res = d7.classify_line(line)
+    assert res == "file"
+
+    line = "8504156 c.dat"
+    res = d7.classify_line(line)
+    assert res == "file"
+
+
+def test_find_directories():
+    fs = {
         "/": {"a": {"b.txt": {"size": 14848514}, "c.dat": {"size": 8504156}}, "d": {}}
     }
-    res = d7.create_fs_dictionary(terminal_output)
-    assert res == fs_dictionary
+
+    res = d7.find_directories(fs)
+    assert res == ["/", "a", "d"]
+
+
+# def test_create_fs_dictionary():
+#     terminal_output = """$ cd /
+# $ ls
+# dir a
+# 14848514 b.txt
+# 8504156 c.dat
+# dir d"""
+#     fs_dictionary = {
+#         "/": {"a": {"b.txt": {"size": 14848514}, "c.dat": {"size": 8504156}}, "d": {}}
+#     }
+#     res = d7.create_fs_dictionary(terminal_output)
+#     assert res == fs_dictionary
