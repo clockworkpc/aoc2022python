1d0ffb051a87c55932048c7d746f3c24b5e873d6
Alexander Garber <clockworkpc@gmail.com>
2023-08-25 00:10:09 +0300

    using a class

--- aoc2022/day_07.py
+++ aoc2022/day_07.py
@@ -2,23 +2,172 @@
 
 import re
 import pprint
-import copy
+
+global_counter = 0
+global_directory_depth = 0
+logs = []
 
 
 def pp(x):
     pprint.pprint(x)
 
 
-def classify_line(line):
-    def regex_test(pattern, value):
-        if re.match(pattern, line):
-            return value
+def readlines(path):
+    with open(path, "r") as file:
+        return [line.strip() for line in file]
 
-    return (
-        regex_test(r"^\$", "command")
-        or regex_test(r"^\d+", "file")
-        or regex_test(r"^dir", "directory")
-    )
+
+def classify_line(line):
+    cd_pattern = r"^\$ cd"
+    file_pattern = r"^\d+ [a-z]+"
+    directory_pattern = r"^dir [a-z]+"
+    ls_pattern = r"^\$ ls"
+
+    if re.match(cd_pattern, line):
+        return "cd_command"
+
+    if re.match(file_pattern, line):
+        return "file"
+
+    if re.match(directory_pattern, line):
+        return "directory"
+
+    if re.match(ls_pattern, line):
+        return "ls_command"
+
+    raise ValueError(f"Not classified: {line}")
+
+
+def root_directory():
+    return {
+        "type": "directory",
+        "name": "/",
+        "parents": [],
+        "size": None,
+        "depth": 0,
+        "position": 0,
+    }
+
+
+class FileSystem:
+    def __init__(self):
+        self.fs = [root_directory()]
+        self.current_dir = {}
+        self.parent_dir_names = []
+        self.current_dir_depth = 0
+        self.current_dir_position = 0
+
+    def find_file_or_directory(self, name, parents):
+        return [
+            hsh for hsh in self.fs if hsh["name"] == name and hsh["parents"] == parents
+        ]
+
+    def go_up_fs_tree(self, dir_name):
+        parents = self.parent_dir_names[0:-2]
+        previous_dir = self.find_file_or_directory(dir_name, parents)
+
+        if previous_dir:
+            current_dir = previous_dir
+        else:
+            print(f"Directory {dir_name} not found")
+
+        self.parent_dir_names = previous_dir["parents"]
+        self.current_dir_depth = previous_dir["depth"]
+        self.current_dir_position = previous_dir["position"]
+
+    def go_down_fs_tree(self, dir_name):
+        parents = [self.parent_dir_names].append(dir_name)
+        next_dir = self.find_file_or_directory(dir_name, parents)
+
+        if next_dir:
+            self.current_dir = next_dir
+        else:
+            raise TypeError(f"Directory {dir_name} not found")
+
+        print(f"next_dir: {next_dir}")
+        self.parent_dir_names = next_dir["parents"]
+        self.current_dir_depth = next_dir["depth"]
+        self.current_dir_position = next_dir["position"]
+
+    def find_or_create_directory(self, line):
+        if line.strip() == "$ cd /" and len(self.fs) == 1:
+            self.fs.append(root_directory())
+            self.current_dir = root_directory()
+            return
+
+        dir_name = line.split(" ")[1]
+
+        if self.find_file_or_directory(dir_name, self.parent_dir_names):
+            return
+
+        hsh = self.new_directory(dir_name)
+        self.fs.append(hsh)
+
+    def change_directory(self, line):
+        dir_name = line.split(" ")[2]
+
+        if dir_name == "..":
+            self.go_up_fs_tree(dir_name)
+        if dir_name == "/":
+            self.find_or_create_directory(line)
+        if re.match(r"[a-z]+", dir_name):
+            self.go_down_fs_tree(dir_name)
+
+    def new_directory(self, dir_name):
+        parents = self.current_dir["parents"]
+        parents.append(self.current_dir["name"])
+        hsh = {
+            "type": "directory",
+            "name": dir_name,
+            "parents": parents,
+            "size": None,
+            "depth": self.current_dir["depth"] + 1,
+            "position": self.current_dir["position"] + 1,
+        }
+        return hsh
+
+    def new_file(self, name, size, parents):
+        hsh = {
+            "type": "file",
+            "name": name,
+            "parents": parents,
+            "size": size,
+            "depth": self.current_dir["depth"] + 1,
+            "position": self.current_dir["position"] + 1,
+        }
+        return hsh
+
+    def create_file(self, line):
+        split = line.strip().split(" ")
+        file_name = split[1]
+        size = split[0]
+        parents = [self.current_dir["parents"]].append(
+            self.current_dir["name"])
+
+        existing_file = self.find_file_or_directory(file_name, parents)
+        if existing_file:
+            return existing_file
+
+        hsh = self.new_file(file_name, size, parents)
+        self.fs.append(hsh)
+
+    def generate_filesystem(self, path):
+        lines = readlines(path)
+
+        for line in lines:
+            print(line)
+            classification = classify_line(line)
+
+            if classification == "cd_command":
+                self.change_directory(line)
+            elif classification == "directory":
+                self.find_or_create_directory(line)
+            elif classification == "file":
+                self.create_file(line)
+            else:
+                continue
+
+        return self.fs
 
 
 def find_parent(dictionary, target_value, path=None):
@@ -38,11 +187,6 @@ def find_parent(dictionary, target_value, path=None):
     return None
 
 
-def readlines(path):
-    with open(path, "r") as file:
-        return [line.strip() for line in file]
-
-
 def go_to_parent_dir(fs, ary):
     cwd = fs
     for dirname in ary[0:-1]:
@@ -50,64 +194,158 @@ def go_to_parent_dir(fs, ary):
     return cwd
 
 
-def generate_filesystem(path):
-    def directory_hash(name, parent):
-        return {"type": "directory", "name": name, "parent": parent, "size": None}
+def change_directory_update_parents(
+    line, current_dir, parent_dir_names, fs, directory_depth
+):
+    print(line)
 
-    def file_hash(name, parent, size):
-        return {"type": "file", "name": name, "parent": parent, "size": size}
+    def find_dir(fs, previous_dir_name, directory_depth, parents):
+        return next(
+            (
+                hsh
+                for hsh in fs
+                if (
+                    hsh["name"] == dir_name
+                    and hsh["directory_depth"] == directory_depth
+                    and hsh["parents"] == parents
+                )
+            ),
+            None,
+        )
 
-    root_dir_hash = directory_hash("/", None)
-    fs = [root_dir_hash]
-    parent_dirs = []
-    working_dirs = ["/"]
+    def set_current_directory(fs, dir_name, directory_depth, parents):
+        root = dir_name == "/"
+        if root:
+            return root_directory()
+        else:
+            current_dir = find_dir(fs, dir_name, directory_depth, parents)
+            if not current_dir:
+                print("No current dir")
+            return current_dir
 
-    def cpd():
-        return parent_dirs[-1]
+    def set_parent_directories(current_dir, parent_dir_names):
+        root = dir_name == "/"
 
-    def cwd():
-        return working_dirs[-1]
+        if root:
+            parent_dir_names = []
+        else:
+            if not current_dir:
+                self.parent_dir_names.append(self.current_dir["name"])
 
-    lines = readlines(path)
+        return parent_dir_names
 
-    for line in lines:
-        line_type = classify_line(line)
+    split = line.split(" ")
+    dir_name = split[2]
 
-        if line_type == "directory":
-            name = line.split(" ")[1]
-            hsh = directory_hash(name, cwd())
-            fs.append(hsh)
-        elif line_type == "file":
-            split = line.split(" ")
-            name = split[1]
-            size = int(split[0])
-            hsh = file_hash(name, cwd(), size)
-            fs.append(hsh)
-        elif line_type == "command":
-            split = line.split(" ")
-            verb = split[1]
-            if verb == "ls":
-                continue
-            elif verb == "cd":
-                destination = split[2]
+    if re.match(r"/", dir_name):
+        directory_depth = 0
+        current_dir = root_directory()
+        parent_dir_names = []
+    elif re.match(r"[a-z]+", dir_name):
+        directory_depth += 1
+        current_dir = set_current_directory(
+            fs, dir_name, directory_depth, parent_dir_names
+        )
 
-                if destination != "..":
-                    new_parent_dir = cwd()
-                    new_working_dir = destination
+        parent_dir_names = set_parent_directories(
+            current_dir, parent_dir_names)
 
-                    parent_dirs.append(new_parent_dir)
-                    working_dirs.append(new_working_dir)
-                elif destination == "..":
-                    last_file_or_directory = fs[-1]
-                    new_working_dir_name = last_file_or_directory["parent"]
-                    working_dirs.append(new_working_dir_name)
+    elif re.match(r"\.\.", dir_name):
+        directory_depth -= 1
+        previous_dir_name = current_dir["parents"][-1]
+        previous_parent_dir_names = parent_dir_names[0:-1]
 
-                    new_working_dir = [
-                        hsh for hsh in fs if hsh["name"] == new_working_dir_name
-                    ][0]
-                    new_parent_dir = new_working_dir["parent"]
-                    parent_dirs.append(new_parent_dir)
-    return fs
+        current_dir = find_dir(
+            fs, previous_dir_name, directory_depth, previous_parent_dir_names
+        )
+        parent_dir_names = previous_parent_dir_names
+
+    return [current_dir, parent_dir_names, directory_depth]
+
+
+def is_root(dir_name):
+    return dir_name == "/"
+
+
+# def change_directory(fs, dir_name, current_dir, parent_dir_names, dir_depth):
+#     if is_root:
+#         current_dir = root_directory()
+#         parent_dir_names = []
+#         dir_depth = 0
+#     else:
+#         current_dir = find_dir(fs, dir_name, dir_depth)
+#         parent_dir_names = current_dir["parents"]
+#         dir_depth = current_dir[""]
+
+
+# def generate_filesystem(path):
+#     def find_or_create_directory(line, current_dir, parent_dir_names, directory_depth):
+#         name = line.split(" ")[1]
+#         if name == "/":
+#             return root_directory()
+#         else:
+#             parent = current_dir["name"]
+#             if not parent in parent_dir_names:
+#                 parent_dir_names.append(parent)
+
+#         parents = list(parent_dir_names)
+
+#         hsh = {
+#             "type": "directory",
+#             "name": name,
+#             "parents": parents,
+#             "size": None,
+#             "directory_depth": directory_depth,
+#         }
+
+#         return hsh
+
+#     def create_file(line, parent_dir_names):
+#         split = line.split(" ")
+#         name = split[1]
+#         size = int(split[0])
+#         parents = list(parent_dir_names)
+#         return {"type": "file", "name": name, "parents": parents, "size": size}
+
+#     current_dir = None
+#     parent_dir_names = []
+#     directory_depth = 0
+
+#     fs = [find_or_create_directory("dir /", current_dir,
+#                            parent_dir_names, directory_depth)]
+
+#     lines = readlines(path)
+
+#     for line in lines:
+#         line_type = classify_line(line)
+
+#         if line_type == "command":
+#             if line.split(" ")[1] == "ls":
+#                 continue
+#             elif line.split(" ")[1] == "cd":
+#                 (
+#                     current_dir,
+#                     parent_dir_names,
+#                     directory_depth,
+#                 ) = change_directory_update_parents(
+#                     line, current_dir, parent_dir_names, fs, directory_depth
+#                 )
+#                 print(f"parents: {current_dir['parents']}")
+#                 print(f"parents count: { len(current_dir['parents']) }")
+#                 print(f"directory_depth: {directory_depth}\n")
+
+#         elif line_type == "directory":
+#             hsh = create_directory(
+#                 line, current_dir, parent_dir_names, directory_depth)
+#             if hsh["name"] == "zfl":
+#                 my_list = [hsh for hsh in fs if hsh["name"] == "zfl"]
+#             fs.append(hsh)
+
+#         elif line_type == "file":
+#             hsh = create_file(line, parent_dir_names)
+#             fs.append(hsh)
+
+#     return fs
 
 
 # def get_all_dirs(fs, dirname):
@@ -122,28 +360,38 @@ def generate_filesystem(path):
 
 
 def directory_size(fs, dirname):
-    clone_fs = copy.copy(fs)
-    print(f"LEN FS: {len(clone_fs)}")
-    all_dirs = [hsh for hsh in clone_fs if hsh["type"] == "directory"]
-    dirnames = [dirname]
+    all_files = [
+        hsh for hsh in fs if hsh["type"] == "file" and dirname in hsh["parents"]
+    ]
 
-    for i in range(len(all_dirs)):
-        for hsh in all_dirs:
-            print(hsh)
-            if hsh["parent"] in dirnames:
-                dirnames.append(hsh["parent"])
-                all_dirs.remove(hsh)
+    sizes = list(map(lambda hsh: hsh["size"], all_files))
 
-    for hsh in fs:
-        if hsh["type"] == "directory" and hsh["parent"] in dirnames:
-            dirnames.append(hsh["name"])
+    return sum(sizes)
 
-    files = [hsh for hsh in fs if (
-        hsh["parent"] in dirnames and hsh["type"] == "file")]
 
-    file_sizes = list(map(lambda hsh: hsh["size"], files))
+# def directory_size(fs, dirname):
+#     clone_fs = copy.copy(fs)
+#
+#     all_dirs = [hsh for hsh in clone_fs if hsh["type"] == "directory"]
+#     dirnames = [dirname]
 
-    return sum(file_sizes)
+#     for i in range(len(all_dirs)):
+#         for hsh in all_dirs:
+#
+#             if hsh["parent"] in dirnames:
+#                 dirnames.append(hsh["parent"])
+#                 all_dirs.remove(hsh)
+
+#     for hsh in fs:
+#         if hsh["type"] == "directory" and hsh["parent"] in dirnames:
+#             dirnames.append(hsh["name"])
+
+#     files = [hsh for hsh in fs if (
+#         hsh["parent"] in dirnames and hsh["type"] == "file")]
+
+#     file_sizes = list(map(lambda hsh: hsh["size"], files))
+
+#     return sum(file_sizes)
 
 
 def collect_dir_sizes(fs, limit=None):
--- tests/test_day_07.py
+++ tests/test_day_07.py
@@ -8,6 +8,15 @@ INPUT_TEST_PATH = "tests/input_files/day_07_input_sample.txt"
 INPUT_FULL_PATH = "tests/input_files/day_07_input.txt"
 
 
+def pp(x):
+    pprint.pprint(x)
+
+
+def sample_lines():
+    with open(INPUT_TEST_PATH, "r") as file:
+        return [line for line in file]
+
+
 def sample_fs():
     with open("tests/fixtures/day_07_fs.json", "r") as json_file:
         return json.load(json_file)
@@ -35,49 +44,64 @@ def test_classify_line():
     assert res == "file"
 
 
+# def test_log():
+#     lines = sample_lines()
+#     for line in lines:
+#         log = d7.update_logs(line)
+#         print(log)
+
+#     res = d7.logs
+#     assert res == "hello"
+
+
 def test_generate_filesystem():
     def sort_nested_dicts(fs):
         return sorted(fs, key=lambda x: x["name"])
 
-    res = d7.generate_filesystem(INPUT_TEST_PATH)
+    obj = d7.FileSystem()
+    res = obj.generate_filesystem(INPUT_TEST_PATH)
     for index, hsh in enumerate(sort_nested_dicts(res), start=0):
         test_hsh = sort_nested_dicts(sample_fs())[index]
+        print(hsh)
+        print(test_hsh)
         assert hsh == test_hsh
 
 
-def test_directory_size():
-    fs = d7.generate_filesystem(INPUT_TEST_PATH)
+# def test_directory_size():
+#     fs = d7.generate_filesystem(INPUT_TEST_PATH)
 
-    res = d7.directory_size(fs, "e")
-    assert res == 584
+#     res = d7.directory_size(fs, "e")
+#     assert res == 584
 
-    res = d7.directory_size(fs, "d")
-    assert res == 4060174 + 8033020 + 5626152 + 7214296
-    assert res == 24933642
+#     res = d7.directory_size(fs, "d")
+#     assert res == 4060174 + 8033020 + 5626152 + 7214296
+#     assert res == 24933642
 
-    res = d7.directory_size(fs, "a")
-    assert res == 94853
+#     res = d7.directory_size(fs, "a")
+#     assert res == 94853
 
-    res = d7.directory_size(fs, "/")
-    assert res == 48381165
-
-
-def test_collect_dir_sizes():
-    fs = d7.generate_filesystem(INPUT_TEST_PATH)
-    res = d7.collect_dir_sizes(fs, limit=None)
-    test = {"/": 48381165, "a": 94853, "d": 24933642, "e": 584}
-    assert res == test
-
-    res = d7.collect_dir_sizes(fs, limit=100000)
-    test = {"a": 94853, "e": 584}
-    assert res == test
+#     res = d7.directory_size(fs, "/")
+#     assert res == 48381165
 
 
-def test_sum_collect_dir_sizes():
-    fs = d7.generate_filesystem(INPUT_TEST_PATH)
-    res = d7.sum_collect_dir_sizes(fs, limit=100000)
-    assert res == 94853 + 584
+# def test_collect_dir_sizes():
+#     fs = d7.generate_filesystem(INPUT_TEST_PATH)
+#     res = d7.collect_dir_sizes(fs, limit=None)
+#     test = {"/": 48381165, "a": 94853, "d": 24933642, "e": 584}
+#     assert res == test
 
-    fs = d7.generate_filesystem(INPUT_FULL_PATH)
-    res = d7.sum_collect_dir_sizes(fs, limit=100000)
-    assert res == 95437
+#     res = d7.collect_dir_sizes(fs, limit=100000)
+#     test = {"a": 94853, "e": 584}
+#     assert res == test
+
+# def test_generate_filesystem_full():
+
+
+# def test_sum_collect_dir_sizes():
+# fs = d7.generate_filesystem(INPUT_TEST_PATH)
+# res = d7.sum_collect_dir_sizes(fs, limit=100000)
+# assert res == 94853 + 584
+
+# fs = d7.generate_filesystem(INPUT_FULL_PATH)
+# res = d7.sum_collect_dir_sizes(fs, limit=100000)
+# assert res == 95437
